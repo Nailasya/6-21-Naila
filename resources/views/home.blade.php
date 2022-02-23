0217bee99c88ca0160d4990e9e22af514c68396b
Nailasya <3103120157@student.smktelkom-pwt.sch.id>
2022-02-23 15:12:34 +0700

    laravel

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,8 +1,23 @@
-@extends('layout.main')
+@extends('layouts.app')
 
-@section('container')
-    <h1> Halaman About </h1>
-    <h3> Naila Syakirotul Rizkiyah </h3>
-    <p> syakirotulnaila@gmail.com </p>
-    <img src="images/naisyaa.jpg" alt="naisyaa" width="200px">
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">{{ __('Dashboard') }}</div>
+
+                <div class="card-body">
+                    @if (session('status'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+
+                    {{ __('You are logged in!') }}
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 @endsection
